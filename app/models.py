43c4db5dc584be40b33ec42690364a2d81905381
Prashanth1602 <prashanthsurapaneni123@gmail.com>
2025-07-26 16:02:44 +0530

    fix: Add ForeignKeyConstraint import

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
